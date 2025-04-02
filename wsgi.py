1e5f32b2c279c40029799a717953b3e40851d956
prashikshitsaini <prashikshit.s@gmail.com>
2025-04-01 19:48:49 -0500

    Deployment V3.1

--- wsgi.py
+++ wsgi.py
@@ -1,8 +1,4 @@
-import os
-import logging
 from server import app
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 3000))
-    logging.info(f"Starting server on port {port}")
-    app.run(host="0.0.0.0", port=port)
+    app.run()
